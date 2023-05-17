cbaf800523e57dd6aa65f4ffab71abcc40f796d0
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-17 16:48:45 +0530

    addd

--- src/model/user.js
+++ src/model/user.js
@@ -1,22 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
- first_name : {
-  type:String,
-  require:true
- },
-last_name : {
-   type:String,
-  require:true
-},
-email : {
-   type:String,
-  require:true
-},
-password :{
-  type:String,
-  require:true
-},
+  first_name: {
+    type:String,
+    required:true
+  },
+  last_name: {
+    type:String,
+    required:true
+  },
+  email: {
+    type:String,
+    required:true,
+  },
+  password: {
+    type:String,
+    required:true
+  }
 });
-
 module.exports = mongoose.model("user", userSchema);
